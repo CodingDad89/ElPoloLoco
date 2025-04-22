51d9905a28750d66135d99168015d0ceec5f1318
CodingDad89 <lutz.amgarten@web.de>
2025-04-23 00:15:14 +0200

    the game starts

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let ctx;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    character.src = 'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png';
+    ctx = canvas.getContext('2d');
+    ctx.drawImage(character, 20, 20, 50, 150);
+
+}
