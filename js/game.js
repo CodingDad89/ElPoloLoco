23eaa231c36b7f64f3e52052f9bd4b93e94b481c
CodingDad89 <lutz.amgarten@web.de>
2025-04-23 10:30:39 +0200

    Loading some files and game prep

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ let character = new Image();
 
 function init() {
     canvas = document.getElementById('canvas');
-    character.src = 'img_pollo_locco/img/2_character_pepe/2_walk/W-21.png';
+    character.src = 'img/2_character_pepe/2_walk/W-21.png';
     ctx = canvas.getContext('2d');
     ctx.drawImage(character, 20, 20, 50, 150);
 
