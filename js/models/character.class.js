86d31f462029353e80e4d3454a7b1ed11d32548d
CodingDad89 <lutz.amgarten@web.de>
2025-04-28 14:52:00 +0200

    Array Changes for Char

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -2,18 +2,29 @@ class Character extends MovableObject {
 
     width = 150;
     heigth = 200;
+    imagesWalking = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ]
+    currentImage = 0;
     constructor(path){
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-212.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png',
-        ])
+        this.loadImages(this.imagesWalking)
+        this.animate();
     }
 
+    animate() {
+        setInterval(() => {
+               let path = this.imagesWalking[this.currentImage];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        }, 1000);
+     
+    }
 
     jump() {
 
